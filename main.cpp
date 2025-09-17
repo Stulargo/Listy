9136e94a2f6f9f8f7b7c78fc6fb32b1d08e05c5f
Stulargo <114095328+Stulargo@users.noreply.github.com>
2025-09-17 11:29:23 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,41 @@
 #include <iostream>
+ 
 using namespace std;
-
-struct Osoba {
-    string nazwisko;
-    int wiek;
-    Osoba *next;
+//strugktura
+struct Osoba{
+string nazwisko;
+int wiek;
+Osoba *next;
+ 
+ 
+ 
 };
-
-int main() {
-    Osoba *head = NULL;
-    Osoba *nowy = new Osoba;
-
-    cout << "Podaj nazwisko: ";
-    cin >> nowy->nazwisko;
-    cout << "Podaj wiek: ";
-    cin >> nowy->wiek;
-
-    nowy->next = head;
-    head = nowy;
-
-    Osoba *temp = head;
-    while (temp != NULL) {
-        cout << "Nazwisko: " << temp->nazwisko << ", Wiek: " << temp->wiek << endl;
-        temp = temp->next;
-    }
-
+ 
+int main()
+{
+Osoba *head;
+Osoba *nowy;
+char d;
+do{
+nowy = new Osoba;
+cout << "podaj nazwisko" << endl;
+cin >> nowy->nazwisko;
+cout << "podaj wiek " << endl;
+cin >> nowy->wiek;
+nowy->next=NULL;
+head=nowy;
+cout << "Czy chcesz wprowadziæ nowy element do listy? (T/N)" << endl;
+cin>>d;
+}
+while(d=='t');
+ cout << "otrzymana lista:" << endl;
+ Osoba *temp;
+ temp=head;
+ 
+ while(temp!=NULL){
+ cout << temp->nazwisko << " "<<temp->wiek << endl;
+ temp = temp->next;
+ }
     return 0;
 }
+ 
