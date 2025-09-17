cc95a6a17faae1e11e38de5d9d7a0ea405919602
Stulargo <114095328+Stulargo@users.noreply.github.com>
2025-09-17 11:36:29 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
- 
+
 using namespace std;
-//strugktura
+
 struct Osoba{
 string nazwisko;
 int wiek;
 Osoba *next;
- 
- 
- 
+
+
+
 };
- 
+
 int main()
 {
-Osoba *head;
+Osoba *head = NULL;
 Osoba *nowy;
 char d;
 do{
@@ -22,20 +22,20 @@ cout << "podaj nazwisko" << endl;
 cin >> nowy->nazwisko;
 cout << "podaj wiek " << endl;
 cin >> nowy->wiek;
-nowy->next=NULL;
+nowy->next=head;
 head=nowy;
-cout << "Czy chcesz wprowadziæ nowy element do listy? (T/N)" << endl;
+cout << "Czy chcesz wprowadzic nowy element do listy? (T/N)" << endl;
 cin>>d;
 }
 while(d=='t');
  cout << "otrzymana lista:" << endl;
  Osoba *temp;
  temp=head;
- 
+
  while(temp!=NULL){
  cout << temp->nazwisko << " "<<temp->wiek << endl;
  temp = temp->next;
  }
     return 0;
 }
- 
+
